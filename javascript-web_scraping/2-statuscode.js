c8bb5bdfbae1076aac57cd242466fbde70867bac
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-13 14:39:07 +1000

    chore: remove a white space

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -8,7 +8,7 @@ if (args.length !== 3) {
 }
 request(args[2], function (error, response, body) {
   if (error) {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });
