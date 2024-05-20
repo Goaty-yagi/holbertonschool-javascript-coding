43e6241aa625bf60350b5359a75a7997833b6e75
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-20 16:19:24 +1000

    feat(task 2): add function countStudents in the file 2-read_file.js.

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function messageGenerator(array, field = '') {
+  if (!Array.isArray(array)) {
+    throw new Error('array is empty.');
+  }
+  const message = 'Number of students:';
+  if (!field) {
+    return `${message} ${array.length}`;
+  }
+  const messageWithField = `Number of students in ${field}:`;
+  const firstnameList = [];
+  for (const line of array) {
+    const student = line.split(',');
+    if (student[3] === field) {
+      firstnameList.push(student[0]);
+    }
+  }
+  return `${messageWithField} ${firstnameList.length}. list: ${firstnameList.join(', ')}`;
+}
+
+function countStudents(path) {
+  try {
+    const file = fs.readFileSync(path, 'utf8').trim();
+    const lines = file.split('\n').slice(1);
+    console.log(messageGenerator(lines));
+    console.log(messageGenerator(lines, 'CS'));
+    console.log(messageGenerator(lines, 'SWE'));
+  } catch (error) {
+    throw Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
