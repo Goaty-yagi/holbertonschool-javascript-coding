082626bed35a85b4be5bece1c3a5133a272da0b8
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-20 20:56:46 +1000

    chore: remove a new line

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const http = require('http');
 const PORT = 1245;
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello Holberton School!\n');
+  res.write('Hello Holberton School!');
   res.end();
 })
 
