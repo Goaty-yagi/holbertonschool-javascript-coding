f3da3ccf6a0e0a70d384d2a6facf3e3fff5c6432
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-20 22:27:54 +1000

    feat(task 5): add a file named 5-http.js, create a small HTTP server using the http module.

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,70 @@
+const http = require('http');
+const fs = require('fs');
+
+function messageGenerator(array, field = '') {
+  if (!Array.isArray(array)) {
+    throw new Error('array is empty.');
+  }
+  const message = 'Number of students:';
+  if (!field) {
+    return `${message} ${array.length}`;
+  }
+  const messageWithField = `Number of students in ${field}:`;
+  const firstnameList = [];
+  for (const line of array) {
+    const student = line.split(',');
+    if (student[3] === field) {
+      firstnameList.push(student[0]);
+    }
+  }
+  return `${messageWithField} ${firstnameList.length}. list: ${firstnameList.join(', ')}`;
+}
+
+function countStudents(path) {
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.trim().split('\n').slice(1);
+      const filteredLines = lines.filter((line) => line.trim() !== '');
+      const array = []
+      array.push(messageGenerator(filteredLines));
+      array.push(messageGenerator(filteredLines, 'CS'));
+      array.push(messageGenerator(filteredLines, 'SWE'));
+      return resolve(array);
+    });
+  });
+}
+
+const PORT = 1245;
+const app = http.createServer((req, res) => {
+  switch (req.url) {
+    case '/':
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      res.write('This is the list of our students\n');
+      countStudents("database.csv")
+        .then((data) => {
+          data.forEach((student, index) => {
+            const newLine = data.length === index + 1 ? '' : '\n'
+            res.write(student + newLine);
+          })
+          res.end();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      break;
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found');
+      break;
+  }
+})
+
+app.listen(PORT);
+module.exports = app;
