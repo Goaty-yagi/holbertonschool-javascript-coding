ea6d2035f1c15881ebf3fbfd199beca9656bcc76
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-21 19:34:16 +1000

    chore: modify all codes

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -20,12 +20,12 @@ function messageGenerator(array, field = '') {
   return `${messageWithField} ${firstnameList.length}. List: ${firstnameList.join(', ')}`;
 }
 
-function countStudents(path) {
+function countStudents(filePath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
+    fs.readFile(filePath, 'utf8', (error, data) => {
       if (error) {
-        console.log("INI, ERROR", path)
-        return reject(new Error('Cannot load the database'));
+        reject(new Error('Cannot load the database'));
+        return;
       }
       const lines = data.trim().split('\n').slice(1);
       const filteredLines = lines.filter((line) => line.trim() !== '');
@@ -42,23 +42,24 @@ const PORT = 1245;
 const app = http.createServer((req, res) => {
   switch (req.url) {
     case '/':
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('Hello Holberton School!');
       break;
     case '/students':
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.write('This is the list of our students\n');
-      countStudents('database.csv')
+      const dbFilePath = process.argv[2];
+      countStudents(dbFilePath)
         .then((data) => {
           data.forEach((student, index) => {
             const newLine = data.length === index + 1 ? '' : '\n';
             res.write(student + newLine);
           });
-        })
-        .catch((error) => {
-          res.write(error.message);
-        })
-        .finally(() => {
           res.end();
         })
+        .catch((error) => {
+          res.end(`Cannot load the database`);
+        });
       break;
     default:
       res.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -67,5 +68,8 @@ const app = http.createServer((req, res) => {
   }
 });
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
 module.exports = app;
