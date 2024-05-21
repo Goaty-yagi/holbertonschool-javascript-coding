dd4f31a1343516a8cb3a0532d786492781094eba
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-21 12:48:26 +1000

    chore: change all codes

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,9 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-rl.on('line', (input) => {
-  process.stdout.write(`Your name is: ${input}\r`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
   process.stdout.write('This important software is now closing\n');
-  rl.close();
+  process.exit();
 });
