2df885d93f6ece7e824b696a8e0965f89b7831f7
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-21 11:09:05 +1000

    chore(checker): replace console.log with process.stdout.

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,10 +5,9 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  process.stdout.write(`Your name is: ${name}\r`);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+rl.on('line', (input) => {
+  process.stdout.write(`Your name is: ${input}\n`);
+  process.stdout.write('This important software is now closing\n');
   rl.close();
-});
-rl.on('close', () => {
-  console.log('This important software is now closing');
 });
