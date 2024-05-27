e2f1fc9f552acba975003036dd24ccd6f409f78c
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-27 14:18:14 +1000

    style(checker): modify styling.

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,17 +1,17 @@
 import fs from 'fs';
 
-export function readDatabase(filePath) {
+export default function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (error, data) => {
       if (error) {
         reject(new Error('Cannot load the database'));
         return;
       }
-      
+
       const lines = data.trim().split('\n').slice(1);
       const students = {};
 
-      lines.forEach(line => {
+      lines.forEach((line) => {
         const [firstname, , , field] = line.split(',');
         if (students[field]) {
           students[field].push(firstname);
