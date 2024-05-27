d35de8719518c4f1541d023062ea2d170253198d
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-05-27 14:34:48 +1000

    style(checker): test commit

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-export function readDatabase(filePath) {
+function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (error, data) => {
       if (error) {
@@ -24,3 +24,5 @@ export function readDatabase(filePath) {
     });
   });
 }
+
+export default readDatabase;
